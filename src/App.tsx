3a6b65f0fe1bdfe2b43193a0a9b2d39e9ebf20d4
JHenrique <henryque.pc.game@gmail.com>
2025-09-25 14:58:06 -0300

    Movendo o Container para seu próprio React Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
 import { Timer } from "lucide-react";
 import Heading from "./components/Heading/Heading";
 import "./styles/global.css";
+import Container from "./components/Container/Container";
 
 export default function App() {
   console.log("Hello App");
   return (
     <>
-      <Heading>
-        Ola Mundo{" "}
-        <button
-          onClick={() =>
-            confirm("Tem ceteza que deseja clickar?")
-              ? alert("Ola Mundo Henrique")
-              : null
-          }
-        >
-          <Timer />
-        </button>
-      </Heading>
+      <Container>
+        <Heading>
+          Ola Mundo{" "}
+          <button
+            onClick={() =>
+              confirm("Tem ceteza que deseja clickar?")
+                ? alert("Ola Mundo Henrique")
+                : null
+            }
+          >
+            <Timer />
+          </button>
+        </Heading>
+      </Container>
     </>
   );
 }
