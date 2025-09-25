0c4272a8113614a5653539df9931ddf7f8895bc9
JHenrique <henryque.pc.game@gmail.com>
2025-09-25 13:08:25 -0300

    Finalização do componente Heading e tipagem TypeScript de children (ReactNode)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
-import './styles/global.css';
+import { Timer } from "lucide-react";
+import Heading from "./components/Heading/Heading";
+import "./styles/global.css";
 
-export default function App()
-{
-    console.log('Hello App')
-    return (
-      <h2 onClick={()=> alert('Ola Mundo Henrique')}>Ola Mundo</h2>
-    );
+export default function App() {
+  console.log("Hello App");
+  return (
+    <>
+      <Heading>
+        Ola Mundo{" "}
+        <button
+          onClick={() =>
+            confirm("Tem ceteza que deseja clickar?")
+              ? alert("Ola Mundo Henrique")
+              : null
+          }
+        >
+          <Timer />
+        </button>
+      </Heading>
+    </>
+  );
 }
