744283a794e62754cd869aaa603de215e737b8c3
JHenrique <henryque.pc.game@gmail.com>
2025-09-27 20:44:48 -0300

    Context API - movendo nosso estado real para dentro do contexto

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,15 @@
 import "./styles/global.css";
 import Home from "./pages/Home";
-import { useState } from "react";
-import { TaskContext } from "./contexts/TaskContext";
-
-const initialState: TaskStateModel = {
-  tasks: [],
-  secondsRemaining: 0,
-  formattedSecondsRemaining: "00:00",
-  activeTask: null,
-  currentCycle: 0,
-  config: {
-    workTime: 25 * 60, // 25 minutes
-    shortBreakTime: 5 * 60, // 5 minutes
-    longBreakTime: 15 * 60, // 15 minutes
-  },
-};
+import { TaskConstextProvider, useTaskContext } from "./contexts/TaskContext";
 
 export default function App() {
-  const [state, setState] = useState(initialState);
+  const taskContext = useTaskContext();
+
+  console.log(taskContext);
 
   return (
-    <TaskContext.Provider value={{ state, setState }}>
+    <TaskConstextProvider>
       <Home />
-    </TaskContext.Provider>
+    </TaskConstextProvider>
   );
 }
--- src/contexts/TaskContext/index.tsx
+++ src/contexts/TaskContext/index.tsx
@@ -1,3 +1,50 @@
-import { createContext } from "react";
+/* eslint-disable react-refresh/only-export-components */
+import { createContext, useContext } from "react";
+import type { TaskStateModel } from "../../models/TaskStateModel";
 
-export const TaskContext = createContext({});
+// Estado inicial
+const initialState: TaskStateModel = {
+  tasks: [],
+  secondsRemaining: 0,
+  formattedSecondsRemaining: "00:00",
+  activeTask: null,
+  currentCycle: 0,
+  config: {
+    workTime: 25 * 60, // 25 minutes
+    shortBreakTime: 5 * 60, // 5 minutes
+    longBreakTime: 15 * 60, // 15 minutes
+  },
+};
+
+// Definindo o tipo do contexto
+type TaskContextProps = {
+  state: TaskStateModel;
+  setState: React.Dispatch<React.SetStateAction<TaskStateModel>>;
+};
+
+// Criando o contexto com valor inicial
+const initialContextValue: TaskContextProps = {
+  state: initialState,
+  setState: () => {},
+};
+
+// Criando o contexto
+export const TaskContext = createContext<TaskContextProps>(initialContextValue);
+
+// Componente para fornecer o contexto
+export function TaskConstextProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <TaskContext.Provider value={{ ...initialContextValue }}>
+      {children}
+    </TaskContext.Provider>
+  );
+}
+
+// Hook personalizado para usar o contexto
+export function useTaskContext() {
+  return useContext(TaskContext);
+}
